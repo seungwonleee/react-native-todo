ec5d5173112ca77cfdff4ac32e1e86bc74b7d94c
seungwonleee <seungwon.code@gmail.com>
2022-01-03 01:08:13 +0900

    Add: todo submit button

--- components/AddTodo.js
+++ components/AddTodo.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import {StyleSheet, View, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+  Keyboard,
+} from 'react-native';
 
 const AddTodo = () => {
+  const [text, setText] = useState('');
+
+  const button = (
+    <View style={styles.button}>
+      <Image source={require('../assets/icons/add_white/add_white.png')} />
+    </View>
+  );
+
+  const onPress = () => {
+    setText('');
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.block}>
-      <TextInput placeholder="할 일을 입력하세요." style={styles.input} />
+      <TextInput
+        placeholder="할 일을 입력하세요."
+        style={styles.input}
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={onPress}
+        returnKeyType="done"
+      />
+      {Platform.select({
+        ios: (
+          <TouchableOpacity activeOpacity={0.5} onPress={onPress}>
+            {button}
+          </TouchableOpacity>
+        ),
+        android: (
+          <View style={styles.circleWrapper}>
+            <TouchableNativeFeedback onPress={onPress}>
+              {button}
+            </TouchableNativeFeedback>
+          </View>
+        ),
+      })}
     </View>
   );
 };
@@ -13,15 +56,30 @@ export default AddTodo;
 
 const styles = StyleSheet.create({
   block: {
+    backgroundColor: '#ffffff',
     height: 64,
     paddingHorizontal: 16,
     borderColor: '#bdbdbd',
     borderTopWidth: 1,
     borderBottomWidth: 1,
-    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   input: {
+    flex: 1,
     fontSize: 16,
     paddingVertical: 8,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 48,
+    height: 48,
+    backgroundColor: '#03a9f4',
+    borderRadius: 24,
+  },
+  circleWrapper: {
+    overflow: 'hidden',
+    borderRadius: 25,
   },
 });
