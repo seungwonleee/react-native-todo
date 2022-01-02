16ddadf8025128bb3927af1c2320bfce07856ea0
seungwonleee <seungwon.code@gmail.com>
2022-01-03 00:18:40 +0900

    Fix: Platform.os -> Platform.select

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import React from 'react';
-import {
-  // SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  KeyboardAvoidingView,
-  Platform,
-} from 'react-native';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
@@ -19,7 +9,7 @@ function App() {
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
         <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          behavior={Platform.select({ios: 'padding', android: undefined})}
           style={styles.avoid}>
           <DateHead />
           <Empty />
