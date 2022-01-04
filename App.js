770a6f25a16188ac73beb5290b51ec5fd0e88610
seungwonleee <seungwon.code@gmail.com>
2022-01-05 07:42:26 +0900

    Add: storage and fix error

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
 import TodoList from './components/TodoList';
+import todosStorage from './storages/todosStorages';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, text: '1번 할 일', done: true},
-    {id: 2, text: '2번 할 일', done: false},
-    {id: 3, text: '3번 할 일', done: false},
-  ]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    todosStorage.get().then(setTodos).catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    todosStorage.set(todos).catch(console.error);
+  }, [todos]);
 
   const onInsert = text => {
     // 새로 등록할 항목의 id를 구합니다.
     // 등록된 항목 중에서 가장 큰 id를 구하고, 그 값에 1을 더합니다.
     // 만약 리스트가 비어있다면 1을 id로 사용합니다.
     const nextId =
-      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+      todos?.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
     const todo = {
       id: nextId,
       text,
@@ -37,7 +42,6 @@ function App() {
 
   const onRemove = id => {
     const nextTodos = todos.filter(todo => todo.id !== id);
-    console.log(nextTodos);
     setTodos(nextTodos);
   };
 
@@ -48,10 +52,10 @@ function App() {
           behavior={Platform.select({ios: 'padding', android: undefined})}
           style={styles.avoid}>
           <DateHead />
-          {todos.length > 1 ? (
-            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
-          ) : (
+          {todos.length === 0 ? (
             <Empty />
+          ) : (
+            <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
           )}
           <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
